131633e6b98a17626920aee0b43c8a895b35b031
Yaseen Kola <yaseenkola@gmail.com>
2021-04-03 08:28:57 +0400

    Initial Commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => "Yaseen Kola",
+            'email' => "yaseenkola@gmail.com",
+            'password' => bcrypt('password')
+        ]);
     }
 }
